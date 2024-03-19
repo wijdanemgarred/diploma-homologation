0831c8ffb98ffe9c1955c4ab6ab6f9c32633925c
Aymane <148797091+aBenbou@users.noreply.github.com>
2024-03-19 01:22:55 +0000

    first commit

---
+++ src/main/java/ma/ac/uir/pi2024/Pi2024Application.java
@@ -0,0 +1,13 @@
+package ma.ac.uir.pi2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ac/uir/pi2024/Pi2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.uir.pi2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
